5f27283cbdc150224d1e3fc26d77c16e6109e347
demersson barros <demerson.b@hotmail.com>
2022-11-05 21:10:08 -0300

    page enviar imagem

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,23 @@ function App() {
 
   return (
     <div className="App">
-      <button>ola mundo</button>
+      
+      <header>
+        <div className='flex h-24 justify-center items-center bg-slate-600'>
+          <img className=' h-20 rounded-full'  src="https://s.tmimgcdn.com/scr/1200x627/80800/modelo-de-logotipo-de-logistica_80816-original.jpg"  />
+        </div>
+
+        <main className='flex justify-center items-center flex-col gap-24'>
+            <div className='flex justify-center items-center flex-col gap-8'>
+              <h1 className='mt-16 text-5xl'>Log Deck</h1>
+
+              <h2 className='text-2xl'>Priorização e confiabilidade</h2>
+            </div>
+
+            <input type="file" />
+        </main>
+
+      </header>
     </div>
   )
 }
